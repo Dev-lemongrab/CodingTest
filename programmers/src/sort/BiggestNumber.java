e4df59af3f512bd4129e755bedd6172c97ab83fe
YunSeongOh <gagizzang92@gmail.com>
2021-11-23 23:26:50 +0900

    Programmers Question
    BiggestNumber & NumberK

---
+++ programmers/src/sort/BiggestNumber.java
@@ -0,0 +1,45 @@
+package sort;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+//프로그래머스 가장큰수
+
+public class BiggestNumber {
+	
+	
+	public static void main(String[] args) {
+				String answer = "";
+			
+				int[] numbers = {3, 30, 33, 34,5,9};
+		   
+		        String[] str = new String[numbers.length];
+		    
+		        for(int i = 0 ; i < numbers.length ; i++){
+		            str[i] = String.valueOf(numbers[i]);   
+		        }
+		        
+
+		        
+		        Arrays.sort(str, new Comparator<String>() {
+		        	@Override
+		        	public int compare(String o1, String o2) {
+		        		return (o2+o1).compareTo(o1+o2);
+		        	}
+		        });
+
+		        for(int k = 0 ; k < numbers.length ; k++){
+		            answer += str[k];
+		        }
+		        
+		        
+		        if(str[0].equals("0")){
+		            answer = "0";
+		        }
+		        
+		        System.out.println(answer);
+		       //3,2   23 과  32 비교 -1 내림차순 3,2 그대로
+		        
+		       
+	}
+}
---
+++ programmers/src/sort/NumberK.java
@@ -0,0 +1,28 @@
+package sort;
+
+import java.util.*;
+
+
+public class NumberK {
+	
+	
+	    public int[] solution(int[] array, int[][] commands) {
+	        int k = 0;
+	        int start = 0;
+	        int end = 0;
+	        int[] answer = new int[commands.length];
+	        for(int i = 0 ; i < commands.length ; i++){
+	            start = commands[i][0]-1;
+	            end = commands[i][1];
+	            k = commands[i][2];
+	            int[] tmp = new int[end-start];
+	            for(int j = 0 ; j < tmp.length ; j++) {
+	                tmp[j] = array[start++];
+	            }
+	            Arrays.sort(tmp);
+	            answer[i] = tmp[k-1];
+	        }
+	        return answer;
+	    }
+	
+}
