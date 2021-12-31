7180ddbd75587a83a9f4cb802850132fa94c753f
YunSeongOh <gagizzang92@gmail.com>
2021-12-31 18:53:10 +0900

    버블정렬 시간복잡도 단축
    O(N^) -> O(logN)

--- Goorm/src/sort/Bubblesort.java
+++ Goorm/src/sort/Bubblesort.java
@@ -18,13 +18,16 @@ public class Bubblesort {
 	public static void bubbleSort(int[] arr) {
 		int tempVar = 0;
 		for(int i = 0; i < arr.length-1 ; i++) {
+			boolean flag = true;//정렬이 완료되면 반복문을 끝낼 표식
 			for(int j = 0 ; j<arr.length-1-i ; j++) {
 				if(arr[j]>arr[j+1]) {//앞이 뒤보다 크면
 					tempVar=arr[j];
 					arr[j] = arr[j+1];
 					arr[j+1] = tempVar;//값 바꿔주기
-				}//4,5,3,1,2 -> 4,3,1,2,5 -> 
+					flag = false;//한번이라도 자리교환을 해주면 아직 정렬이 안된것..	
+				}
 			}
+			if(flag) break;//한번도 자리교환을 안해주면 그대로 true일것. 그럼 종료
 		}
 		for(int i = 0; i<arr.length;i++) {
 			System.out.print(arr[i]+" ");
